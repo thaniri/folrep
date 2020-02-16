949bd8b11a3aad70809c98dbb8f707809067ff93
Marek Slabicki <thaniri@gmail.com>
2020-02-15 17:00:56 -0800

    attempting to get travis to push a new image to dockerhub

---
+++ travis/travis-cd-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script is used for deploying new version of folrep merged to master
+#
+
+set -ex
+
+$REPO_USERNAME="folrep"
+$IMAGE_NAME="folrep"
+$IMAGE_TAG="latest"
+
+if [[ -z "${DOCKER_USERNAME}" ]]; then
+  echo "DOCKER_USERNAME is not set"
+  exit 1
+fi
+
+if [[ -z "${DOCKER_PASSWORD}" ]]; then
+  echo "DOCKER_USERNAME is not set"
+  exit 1
+fi
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
+docker build -t "${REPO_USERNAME}"/"${IMAGE_NAME}":"$IMAGE_TAG"
+
+docker push "${REPO_USERNAME}"/"${IMAGE_NAME}":"$IMAGE_TAG"
